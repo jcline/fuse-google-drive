0c5a0957a4281de02d3471b9344a0a2672b536b5
James Cline <james.cline@gatech.edu>
2012-04-25 16:36:28 -0400

    comment operation functions and add a couple missing functions

--- gd_fuse_operations.c
+++ gd_fuse_operations.c
@@ -19,209 +19,326 @@
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
 
+/** Get file attributes.
+ *
+ */
 int gd_getattr (const char *path, struct stat *statbuf)
 {
 	return 0;
 }
 
-
+/** Read the target of a symbolic link.
+ *
+ */
 int gd_readlink (const char *path, char *link, size_t size)
 {
 	return 0;
 }
 
-
+/** Create a regular file.
+ *
+ */
 int gd_mknod (const char *path, mode_t mode, dev_t dev)
 {
 	return 0;
 }
 
-
+/** Create a directory.
+ *
+ */
 int gd_mkdir (const char *path, mode_t mode)
 {
 	return 0;
 }
 
-
+/** Remove a file.
+ *
+ */
 int gd_unlink (const char *path)
 {
 	return 0;
 }
 
-
+/** Remove a directory.
+ *
+ */
 int gd_rmdir (const char *path)
 {
 	return 0;
 }
 
-
+/** Create a symbolic link.
+ *
+ *  Google Drive likely does not support an equivalent operation.
+ *  We could allow this for a specific session, but it would be lost
+ *  after an unmount.
+ */
 int gd_symlink (const char *path, const char *link)
 {
 	return 0;
 }
 
-
+/** Rename a file.
+ *
+ */
 int gd_rename (const char *path, const char *newpath)
 {
 	return 0;
 }
 
-
+/** Create a hard link to a file.
+ *
+ *  Google Drive likely does not support an equivalent operation.
+ *  We could allow this for a specific session, but it would be lost
+ *  after an unmount.
+ */
 int gd_link (const char *path, const char *newpath)
 {
 	return 0;
 }
 
-
+/** Change the permission bits of a file.
+ *
+ *  Could this be used to share a document with others?
+ *  Perhaps o+r would make visible to all, o+rw would be editable by all?
+ *  I can't think of a way to do any sort of group level permissions sanely atm.
+ *
+ *  Alternatively, these permission settings could be used for local acces only,
+ *  which would let you make a file locally readable or modifiable by another
+ *  system user. This may violate the principle of least astonishment least.
+ */
 int gd_chmod (const char *path, mode_t mode)
 {
 	return 0;
 }
 
-
+/** Change the owner and group of a file.
+ *
+ *  Since this uses gid and uids, I cannot think of a way to easily use this for
+ *  Google Drive sharing purposes with specific users right now.
+ *  Perhaps some additional utility could be used to display a mapping of
+ *  uid/gids and Google Drive users you can share with?
+ *
+ *  Alternatively, these uid/gid settings could be used for local acces only,
+ *  which would let you make a file locally readable or modifiable by another
+ *  system user. This may violate the principle of least astonishment least.
+ */
 int gd_chown (const char *path, uid_t uid, gid_t gid)
 {
 	return 0;
 }
 
-
+/** Change the size of a file.
+ *
+ */
 int gd_truncate (const char *path, off_t newsize)
 {
 	return 0;
 }
 
-
+/** File open operation.
+ *
+ */
 int gd_open (const char *path, struct fuse_file_info * fileinfo)
 {
 	return 0;
 }
 
-
+/** Read data from an open file.
+ *
+ */
 int gd_read (const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
-
+/** Write data to an open file.
+ *
+ */
 int gd_write (const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
-
+/** Get file system statistics.
+ *
+ */
 int gd_statfs (const char *path, struct statvfs *statv)
 {
 	return 0;
 }
 
-
+/** Possibly flush cached data.
+ *
+ */
 int gd_flush (const char *path, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
-
+/** Release an open file.
+ *
+ */
 int gd_release (const char *path, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
-
+/** Synchronize file contents.
+ *
+ */
 int gd_fsync (const char *path, int datasync, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
-
+/** Set extended attributes.
+ *
+ *  Does this mean anything for Google Drive?
+ */
 int gd_setxattr (const char *path, const char *name, const char *value, size_t size, int flags)
 {
 	return 0;
 }
 
-
+/** Get extended attributes.
+ *
+ *  Does this mean anything for Google Drive?
+ */
 int gd_getxattr (const char *path, const char *name, char *value, size_t size)
 {
 	return 0;
 }
 
-
+/** List extended attributes.
+ *
+ *  Does this mean anything for Google Drive?
+ */
 int gd_listxattr (const char *path, char *list, size_t size)
 {
 	return 0;
 }
 
-
+/** Remove extended attributes.
+ *
+ *  Does this mean anything for Google Drive?
+ */
 int gd_removexattr (const char *path, const char *name)
 {
 	return 0;
 }
 
-
+/** Open a directory.
+ *
+ */
 int gd_opendir (const char *path, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
+/** Read directory.
+ *
+ */
+int gd_readdir (const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileinfo)
+{
+	return 0;
+}
 
+/** Release directory.
+ *
+ */
 int gd_releasedir (const char *path, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
+/** Synchronize directory contents.
+ *
+ */
+int gd_fsyncdir (const char *path, int datasync, struct fuse_file_info *fileinfo)
+{
+	return 0;
+}
 
+/** Initialize filesystem
+ *
+ */
 void *gd_init (struct fuse_conn_info *conn)
 {
 	return NULL;
 }
 
-
+/** Clean up filesystem
+ *
+ *  Automatically called by fuse on filesystem exit (unmount).
+ */
 void gd_destroy (void *userdata)
 {
 }
 
-
+/** Check file access permission.
+ *
+ */
 int gd_access (const char *path, int mask)
 {
 	return 0;
 }
 
-
+/** Create and open a file.
+ *
+ */
 int gd_create (const char *path, mode_t mode, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
-
+/** Change the size of an open file.
+ *
+ */
 int gd_ftruncate (const char *path, off_t offset, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
-
+/** Get attributes from an open file.
+ *
+ */
 int gd_fgetattr (const char *path, struct stat *statbuf, struct fuse_file_info *fileinfo)
 {
 	return 0;
 }
 
-
+/** Perform POSIX file locking operation.
+ *
+ *  Does this make any sense for Google Drive?
+ */
 int gd_lock (const char *path, struct fuse_file_info *fileinfo, int cmd, struct flock *lock)
 {
 	return 0;
 }
 
-
+/** Change the access and modification times of a file with nanosecond
+ *  resoluton.
+ *
+ *  Does this make any sense for Google Drive?
+ */
 int gd_utimens (const char *path, const struct timespec tv[2])
 {
 	return 0;
 }
 
-
+/** Ioctl
+ *
+ *  Does this make any sense for Google Drive?
+ */
 int gd_ioctl (const char *path, int cmd, void *arg, struct fuse_file_info *fileinfo, unsigned int flags, void *data)
 {
 	return 0;
 }
 
-
+/** Poll for IO readiness events.
+ *
+ */
 int gd_poll (const char *path, struct fuse_file_info *fileinfo, struct fuse_pollhandle *ph, unsigned *reventsp)
 {
 	return 0;
