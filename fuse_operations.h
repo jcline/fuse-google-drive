28805b28c4c24da23d0af4e81c4e3b4c9d4b706e
James Cline <james.cline@gatech.edu>
2012-04-24 21:29:46 -0400

    stub out fuse operations

--- fuse_operations.h
+++ fuse_operations.h
@@ -16,3 +16,62 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
+#ifndef GD_FUSE_OPERATIONS_H
+#define GD_FUSE_OPERATIONS_H
+
+int gd_getattr (const char *, struct stat *);
+int gd_readlink (const char *, char *, size_t);
+int gd_getdir (const char *, fuse_dirh_t, fuse_dirfil_t);
+int gd_mknod (const char *, mode_t, dev_t);
+int gd_mkdir (const char *, mode_t);
+int gd_unlink (const char *);
+int gd_rmdir (const char *);
+int gd_symlink (const char *, const char *);
+int gd_rename (const char *, const char *);
+int gd_link (const char *, const char *);
+int gd_chmod (const char *, mode_t);
+int gd_chown (const char *, uid_t, gid_t);
+int gd_truncate (const char *, off_t);
+int gd_utime (const char *, struct utimbuf *);
+int gd_open (const char *, struct fuse_file_info *);
+int gd_read (const char *, char *, size_t, off_t, struct fuse_file_info *);
+int gd_write (const char *, const char *, size_t, off_t,struct fuse_file_info *);
+int gd_statfs (const char *, struct statfs *);
+int gd_flush (const char *, struct fuse_file_info *);
+int gd_release (const char *, struct fuse_file_info *);
+int gd_fsync (const char *, int, struct fuse_file_info *);
+int gd_setxattr (const char *, const char *, const char *, size_t, int);
+int gd_getxattr (const char *, const char *, char *, size_t);
+int gd_listxattr (const char *, char *, size_t);
+int gd_removexattr (const char *, const char *);
+
+struct fuse_operations gd_oper = {
+	//.getattr = gd_getattr;
+	//.readlink = gd_readlink;
+	//.getdir = gd_getdir;
+	//.mknod = gd_mknod;
+	//.mkdir = gd_mkdir;
+	//.unlink = gd_unlink;
+	//.rmdir = gd_rmdir;
+	//.symlink = gd_symlink;
+	//.rename = gd_rename;
+	//.link = gd_link;
+	//.chmod = gd_chmod;
+	//.chown = gd_chown;
+	//.truncate = gd_truncate;
+	//.utime = gd_utime;
+	//.open = gd_open;
+	//.read = gd_read;
+	//.write = gd_write;
+	//.statfs = gd_statfs;
+	//.flush = gd_flush;
+	//.release = gd_release;
+	//.fsync = gd_fsync;
+	//.setxattr = gd_setxattr;
+	//.getxattr = gd_getxattr;
+	//.listxattr = gd_listxattr;
+	//.removexattr = gd_removexattr;
+}
+
+#endif
